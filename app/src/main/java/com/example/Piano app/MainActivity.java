cf82792e458ac837ca68081f37ffa8c751df1781
Badrinath reddy <cvbadarinathreddy@gmail.com>
2021-10-31 17:12:38 +0530

    Piano app

---
+++ app/src/main/java/com/example/Piano app/MainActivity.java
@@ -0,0 +1,159 @@
+package com.example.androidappdevelopmentaugustminorproject;
+
+import android.media.MediaPlayer;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mp;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void onAboutButtonClick(View view){
+
+        Intent intent = new Intent(this, com.example.androidappdevelopmentaugustminorproject.MainActivity3.class);
+        startActivity(intent);
+    }
+
+    public void onNote1(View view){
+
+        mp=MediaPlayer.create(this,R.raw.note_1);
+        mp.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.start();
+            }
+        });
+
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mp.release();
+            }
+        });
+
+    }
+
+
+    public void onNote2(View view){
+
+        mp=MediaPlayer.create(this,R.raw.note_2);
+        mp.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.start();
+            }
+        });
+
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mp.release();
+            }
+        });
+
+    }
+    public void onNote3(View view){
+
+        mp=MediaPlayer.create(this,R.raw.note_3);
+        mp.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.start();
+            }
+        });
+
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mp.release();
+            }
+        });
+
+    }
+    public void onNote4(View view){
+
+        mp=MediaPlayer.create(this,R.raw.note_4);
+        mp.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.start();
+            }
+        });
+
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mp.release();
+            }
+        });
+
+    }
+
+    public void onNote5(View view){
+
+        mp=MediaPlayer.create(this,R.raw.note_5);
+        mp.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.start();
+            }
+        });
+
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mp.release();
+            }
+        });
+
+    }
+
+    public void onNote6(View view){
+
+        mp=MediaPlayer.create(this,R.raw.note_6);
+        mp.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.start();
+            }
+        });
+
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mp.release();
+            }
+        });
+
+    }
+
+    public void onNote7(View view){
+
+        mp=MediaPlayer.create(this,R.raw.note_7);
+        mp.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.start();
+            }
+        });
+
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mp.release();
+            }
+        });
+
+    }
+
+}
